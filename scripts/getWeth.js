24ffe49bab540b45b90edf2198cf9e383dd2f41a
b0esium <b0esium@protonmail.com>
2023-08-03 11:13:15 +0200

    Initial commit

---
+++ scripts/getWeth.js
@@ -0,0 +1,18 @@
+const { getNamedAccounts } = require("hardhat")
+
+const AMOUNT = ethers.utils.parseEther("0.02")
+
+async function getWeth() {
+    const { deployer } = await getNamedAccounts()
+    const iWeth = await ethers.getContractAt(
+        "IWeth",
+        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        deployer
+    )
+    const tx = await iWeth.deposit({ value: AMOUNT })
+    await tx.wait(1)
+    const wethBalance = await iWeth.balanceOf(deployer)
+    console.log(`Got ${wethBalance.toString()} WETH`)
+}
+
+module.exports = { getWeth, AMOUNT }
